27cce481dd5c6b516271c414c55e674abc9e43df
Vargynja <aleksi95@hotmail.fi>
2020-03-12 15:08:32 +0200

    npm audit fix

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { View, Text } from "react-native";
+import { View, Text } from "react-native"
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
 
 export default function CameraScreen() {
 
